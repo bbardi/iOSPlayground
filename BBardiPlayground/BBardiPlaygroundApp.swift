723a6a41b20ca9f129678916fee18fe27de1f5e7
Bogdan Bardi <bardibogdan@gmail.com>
2024-01-28 23:27:00 +0200

    Initial Commit

---
+++ BBardiPlayground/BBardiPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBardiPlaygroundApp.swift
+//  BBardiPlayground
+//
+//  Created by Bogdan Bardi on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BBardiPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
